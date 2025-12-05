067e410077b5b3bf5b9f1a7f1c404b152c61bde7
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2025-12-05 13:18:34 +0530

    submit 1

--- src/components/Review.js
+++ src/components/Review.js
@@ -43,10 +43,6 @@ export default function Review() {
     setReview(reviewList.find((r) => r.id === current));
   }, [current]);
 
-  if (!review) {
-    return <strong>No Review Found</strong>;
-  }
-
   function previous() {
     if (current <= 1) {
       return;
